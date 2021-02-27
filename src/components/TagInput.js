4d702f3684cf0f47260fb8a0a316c65a7fb15b8e
Lucas_Yi <423188112@qq.com>
2021-02-28 01:06:20 +1100

    Initial commit

---
+++ src/components/TagInput.js
@@ -0,0 +1,50 @@
+// Renders a tag input field.
+
+// Define a TagInput component and use the useState() hook to initialize an array from tags.
+// Use Array.prototype.map() on the collected nodes to render the list of tags.
+// Define the addTagData method, which will be executed when pressing the Enter key.
+// The addTagData method calls setTagData to add the new tag using the spread (...) operator to prepend the existing tags and add the new tag at the end of the tagData array.
+// Define the removeTagData method, which will be executed on clicking the delete icon in the tag.
+// Use Array.prototype.filter() in the removeTagData method to remove the tag using its index to filter it out from the tagData array.
+
+import '../css/tagInput.css'
+import React, {useState} from "react"
+
+const TagInput = ()=>{
+    const [tagData, setTagData] = useState([])
+    const addTagData=event=>{
+        if(event.target.value !== ''){
+            setTagData([...tagData, event.target.value])
+            event.target.value = ''
+        }
+    }
+    const removeTagData=index=>{
+        setTagData([...tagData.filter((_,tagIndex)=> tagIndex!==index)])
+    }
+
+    return (
+        <div className="tag-input">
+        <ul className="tags">
+            {tagData.map((tag, index)=>{
+                return <li className="tag" key={index}>
+                    <span className="tag-title">{tag}</span>
+                    <span
+                        className="tag-close-icon"
+                        onClick={() => removeTagData(index)}
+                    >
+                        x
+                    </span>
+                </li>
+            })}
+        </ul>
+        <input
+            type="text"
+            onKeyUp={event => (event.key === 'Enter' ? addTagData(event) : null)}
+            placeholder="Press enter to add a tag"
+        />
+        </div>
+        
+    )
+}
+
+export default TagInput 
